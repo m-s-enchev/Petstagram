9cf7d40a60380594a456222666d06740c0164a16
Momchil Enchev <m.s.enchev@gmail.com>
2023-06-08 20:14:42 +0300

    added urls and views

--- Petstagram/pets/views.py
+++ Petstagram/pets/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def add(request):
+    return render(request, template_name='pets/pet-add-page.html')
+
+
+def pet_details(request):
+    return render(request, template_name='pets/pet-details-page.html')
+
+
+def pet_edit(request):
+    return render(request, template_name='pets/pet-edit-page.html')
+
+
+def pet_delete(request):
+    return render(request, template_name='pets/pet-delete-page.html')
+
--- Petstagram/photos/views.py
+++ Petstagram/photos/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def photo_add(request):
+    return render(request, template_name='photos/photo-add-page.html')
+
+
+def photo_details(request):
+    return render(request, template_name='photos/photo-details-page.html')
+
+
+def photo_edit(request):
+    return render(request, template_name='photos/photo-edit-page.html')
+
