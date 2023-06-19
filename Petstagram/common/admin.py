16ee7402f3e04863e046db0a5c268efa8d650a3a
Momchil Enchev <m.s.enchev@gmail.com>
2023-06-19 14:37:53 +0300

    register in admin

--- Petstagram/common/admin.py
+++ Petstagram/common/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from Petstagram.common.models import Comment, Like
+
+
 # Register your models here.
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    pass
+
