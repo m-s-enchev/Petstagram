5b9e9058f3f180ab9737ed43aaca47e7023ba6be
Momchil Enchev <m.s.enchev@gmail.com>
2023-07-18 08:09:19 +0300

    register user in admin

--- Petstagram/accounts/admin.py
+++ Petstagram/accounts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from Petstagram.accounts.models import PetstagramUser
 
 # Register your models here.
+
+
+class PetstagramUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email')
+
+
+admin.site.register(PetstagramUser, PetstagramUserAdmin)
