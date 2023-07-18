680c343124f750b820cab221e9b22449b06e818a
Momchil Enchev <m.s.enchev@gmail.com>
2023-07-18 08:09:48 +0300

    user model

--- Petstagram/accounts/models.py
+++ Petstagram/accounts/models.py
@@ -1,3 +1,20 @@
+from django.core.validators import MinLengthValidator, RegexValidator
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+class PetstagramUser(AbstractUser):
+    first_name = models.CharField(max_length=30,
+                                  validators=[MinLengthValidator(2),
+                                              RegexValidator(regex='[A-Za-z]')])
+    last_name = models.CharField(max_length=30,
+                                 validators=[MinLengthValidator(2),
+                                             RegexValidator(regex='[A-Za-z]')])
+    email = models.EmailField(unique=True)
+    choices = [("male", "Male"), ("female", "Female"), ("do not show", "Do not show")]
+    gender = models.CharField(choices=choices)
+    picture = models.URLField()
+
+
