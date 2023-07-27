06de424c8200228cbcd0ca6f62e2b8786b477d3d
Momchil Enchev <m.s.enchev@gmail.com>
2023-07-27 21:16:31 +0300

    models changed

--- Petstagram/common/models.py
+++ Petstagram/common/models.py
@@ -8,7 +8,7 @@ from Petstagram.photos.models import Photo
 
 class Comment(models.Model):
     text = models.TextField(max_length=300)
-    date_and_time_of_publication = models.DateTimeField(auto_created=True)
+    date_and_time_of_publication = models.DateTimeField(auto_now_add=True)
     to_photo = models.ForeignKey(Photo, on_delete=models.CASCADE)
     user = models.ForeignKey(to=PetstagramUser, on_delete=models.CASCADE)
 
